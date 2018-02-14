760cb6696fd9fb577bca0cbd11cad49eee8bd660
emin_ <ek1710@imperial.ac.uk>
2018-02-14 12:19:36 +0000

    added vector class, refactored calibration

--- sensor/calibration.py
+++ sensor/calibration.py
@@ -1,68 +1,70 @@
 from magnetometer import Magnetometer
 from utime import sleep_ms
+from utility import Vector3D
 import ujson
 import math
 
 class Lock:
     """Takes measurements of magnetometer levels to determine lock status"""
     def __init__(self):
-        self.locked=[0,0,0]
-        self.closed=[0,0,0]
-        self.open=[0,0,0]
+        # self.locked=[0,0,0]
+        # self.closed=[0,0,0]
+        # self.open=[0,0,0]
+        self.locked = Vector3D()
+        self.closed = Vector3D()
+        self.open = Vector3D()
 
-        self.locked_distance=0
-        self.closed_distance=0
-        self.open_distance=0
+        self.locked_distance = Vector3D()
+        self.closed_distance = Vector3D()
+        self.open_distance = Vector3D()
 
         self.logger=DataLogger()
+        self.scale_factor = 20
+
+    def set_scale_factor(self, scale_factor):
+        self.scale_factor = scale_factor
 
     def calibrate_locked(self):
-        self.logger.measure(10, 100)
+        self.logger.measure(100, 30)
         self.logger.std_dev()
-        self.locked=[self.logger.mean_x, self.logger.mean_y, self.logger.mean_z]
-        self.locked_distance = 10*(math.sqrt(self.logger.std_x * self.logger.std_x
-                                            + self.logger.std_y * self.logger.std_y
-                                            + self.logger.std_z * self.logger.std_z))
+
+        self.locked = self.logger.data_mean
+        self.locked_distance = self.logger.data_std_dev.distance(Vector3D(0,0,0))
 
     def calibrate_closed(self):
-        self.logger.measure(10, 100)
+        self.logger.measure(100, 30)
         self.logger.std_dev()
-        self.closed=[self.logger.mean_x, self.logger.mean_y, self.logger.mean_z]
-        self.closed_distance = 10*(math.sqrt(self.logger.std_x * self.logger.std_x
-                                            + self.logger.std_y * self.logger.std_y
-                                            + self.logger.std_z * self.logger.std_z))
+        self.closed=self.logger.data_mean
+        self.closed_distance = self.logger.data_std_dev.distance(Vector3D(0, 0, 0))
 
     def calibrate_open(self):
-        self.logger.measure(10, 100)
+        self.logger.measure(100, 30)
         self.logger.std_dev()
-        self.open=[self.logger.mean_x, self.logger.mean_y, self.logger.mean_z]
-        self.open_distance = 10*math.sqrt(self.logger.std_x**2
-                                            + self.logger.std_y**2
-                                            + self.logger.std_z**2)
+        self.open=self.logger.data_mean
+        self.open_distance = self.logger.data_std_dev.distance(Vector3D(0, 0, 0))
 
     def is_locked(self):
         self.logger.measure(10, 10)
-        (x,y,z) = self.logger.mean()
-        distance = (self.locked[0]-x)**2 + (self.locked[1]-y)**2+(self.locked[2]-z)**2
-        if distance<self.locked_distance:
+        meanpos = self.logger.mean()
+
+        if meanpos.distance(self.locked)<self.locked_distance*self.scale_factor:
             return True
         else:
             return False
 
     def is_closed(self):
         self.logger.measure(10, 10)
-        (x,y,z) = self.logger.mean()
-        distance = (self.closed[0]-x)**2 + (self.closed[1]-y)**2+(self.closed[2]-z)**2
-        if distance<self.closed_distance:
+        meanpos = self.logger.mean()
+
+        if meanpos.distance(self.closed) < self.closed_distance*self.scale_factor:
             return True
         else:
             return False
 
     def is_open(self):
         self.logger.measure(10, 10)
-        (x,y,z) = self.logger.mean()
-        distance = (self.open[0]-x)**2 + (self.open[1]-y)**2+(self.open[2]-z)**2
-        if distance<self.open_distance:
+        meanpos = self.logger.mean()
+        if meanpos.distance(self.closed) < self.closed_distance*self.scale_factor:
             return True
         else:
             return False
@@ -85,61 +87,64 @@ class DataLogger:
             self.magnetometer = Magnetometer()
         else:
             self.magnetometer = magnetometer
-        self.x=[]
-        self.y=[]
-        self.z=[]
+
+        self.data=[]
 
     def measure(self, num_readings, time_ms_per_reading=0):
-        self.x=[]
-        self.y=[]
-        self.z=[]
+        self.data=[]
         """time per reading must be more than 7 ms"""
         if(time_ms_per_reading>7):
             time_sleep = time_ms_per_reading-7
             for i in range(num_readings+2):
                 self.magnetometer.take_measurement()
                 if i>1:
-                    self.x.append(self.magnetometer.x)
-                    self.y.append(self.magnetometer.y)
-                    self.z.append(self.magnetometer.z)
+                    vector = Vector3D()
+                    vector.x = self.magnetometer.x
+                    vector.y = self.magnetometer.y
+                    vector.z = self.magnetometer.z
+                    self.data.append(vector)
                     sleep_ms(time_sleep)
 
         else:
             for i in range(num_readings):
                 self.magnetometer.take_measurement()
                 if i>1:
-                    self.x.append(self.magnetometer.x)
-                    self.y.append(self.magnetometer.y)
-                    self.z.append(self.magnetometer.z)
+                    vector = Vector3D()
+                    vector.x = self.magnetometer.x
+                    vector.y = self.magnetometer.y
+                    vector.z = self.magnetometer.z
+                    self.data.append(vector)
+
+        if num_readings>=10:
+            self.data=self.data[5:]
 
     def get_json(self):
-        data["x"]=self.x
-        data["y"]=self.y
-        data["z"]=self.z
 
-        return ujson.dumps(data)
+        return ujson.dumps(self.data)
 
     def mean(self):
-        if self.x==[] or self.y==[] or self.z==[]:
+        if self.data==[]:
             print("Take measurement first using measure()")
             return 0
+        self.data_mean= Vector3D()
+        for i in self.data:
+            self.data_mean = self.data_mean + i
+        self.data_mean = self.data_mean.mul(1/len(self.data))
 
-        self.mean_x=sum(self.x)/len(self.x)
-        self.mean_y=sum(self.y)/len(self.y)
-        self.mean_z=sum(self.z)/len(self.z)
-
-        return (self.mean_x, self.mean_y, self.mean_z)
+        return self.data_mean
 
     def std_dev(self):
         self.mean()
+        res = Vector3D()
+        for i in range(len(self.data)):
+            res=res+(self.data[i]-self.data_mean)*(self.data[i]-self.data_mean)
 
-        self.std_z = math.sqrt(sum(map(lambda x: (x - self.mean_x) * (x - self.mean_x), self.z)) / len(self.z))
+        res=res.mul(1/len(self.data))
 
-        self.std_y = math.sqrt(sum(map(lambda x: (x - self.mean_y) * (x - self.mean_y), self.y)) / len(self.y))
+        res = res.sqrt()
 
-        self.std_x = math.sqrt(sum(map(lambda x: (x - self.mean_z) * (x - self.mean_z), self.x)) / len(self.x))
-
-        return (self.std_x,self.std_y,self.std_z)
+        self.data_std_dev = res
+        return res
 
 
 
